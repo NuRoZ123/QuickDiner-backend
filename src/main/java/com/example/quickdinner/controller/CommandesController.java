d5485fd1525b4fb97835f31dc5b60798d58fae74
NuRoZ123 <killiangouzien44220@gmail.com>
2023-04-18 09:40:55 +0200

    refactor: repartition des role au controller

---
+++ src/main/java/com/example/quickdinner/controller/CommandesController.java
@@ -0,0 +1,105 @@
+package com.example.quickdinner.controller;
+
+import com.example.quickdinner.model.Commande;
+import com.example.quickdinner.model.Produit;
+import com.example.quickdinner.model.ProduitCommander;
+import com.example.quickdinner.model.Utilisateur;
+import com.example.quickdinner.service.CommandeService;
+import com.example.quickdinner.service.ProduitCommanderService;
+import com.example.quickdinner.service.ProduitService;
+import com.example.quickdinner.service.UtilisateurService;
+import com.example.quickdinner.utils.Jwt;
+import com.example.quickdinner.utils.PairPoduitQuantite;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/api")
+@CrossOrigin(origins = {"*"})
+public class CommandesController {
+
+    private final UtilisateurService utilisateurService;
+    private final CommandeService commandeService;
+    private final ProduitService produitService;
+    private final ProduitCommanderService produitCommanderService;
+
+    public CommandesController(UtilisateurService utilisateurService, CommandeService commandeService,
+                               ProduitService produitService, ProduitCommanderService produitCommanderService) {
+        this.utilisateurService = utilisateurService;
+        this.commandeService = commandeService;
+        this.produitService = produitService;
+        this.produitCommanderService = produitCommanderService;
+    }
+
+    @ApiOperation("Récupère les commandes d'un utilisateur: Si l'utilisateur est un client")
+    @GetMapping("/user/commandes")
+    public ResponseEntity<List<Commande>> getCommandes(@RequestHeader("Authorization") String token) {
+        Optional<Utilisateur> user = Jwt.getUserFromToken(token, utilisateurService);
+        if(user == null || !user.isPresent()) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        Utilisateur connectedUser = user.get();
+
+        if("Client".equals(connectedUser.getRole().getLibelle())) {
+            return ResponseEntity.ok(commandeService.findAllByUtilisateurId(connectedUser.getId()));
+        }
+
+        return ResponseEntity.status(401).body(null);
+    }
+
+    @ApiOperation("Permet à l'utilisateur de passer une commande")
+    @PostMapping("/user/commandes")
+    public ResponseEntity createCommande(@RequestHeader("Authorization") String token,
+                                         @RequestBody List<PairPoduitQuantite> produits) {
+        Optional<Utilisateur> user = Jwt.getUserFromToken(token, utilisateurService);
+        if(user == null || !user.isPresent()) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        Utilisateur connectedUser = user.get();
+
+        if(!"Client".equals(connectedUser.getRole().getLibelle())) {
+            return ResponseEntity.status(401).body(null);
+        }
+
+        Commande commande = Commande.builder()
+                .utilisateur(connectedUser)
+                .build();
+
+        commande = commandeService.save(commande);
+        Map<Integer, Produit> produitsMap = new HashMap<>();
+
+        Commande finalCommande = commande;
+        List<ProduitCommander> produitCommanders = produits.stream()
+                .filter(pairPoduitQuantite -> {
+                    Produit produit = produitService.findById(pairPoduitQuantite.getId()).orElse(null);
+                    if(produit != null) {
+                        produitsMap.put(pairPoduitQuantite.getId(), produit);
+                        return true;
+                    }
+                    return false;
+                })
+                .map(pairPoduitQuantite ->
+                        ProduitCommander.builder()
+                                .commande(finalCommande)
+                                .produit(produitsMap.get(pairPoduitQuantite.getId()))
+                                .quantite(pairPoduitQuantite.getQuantite())
+                                .build())
+                .collect(Collectors.toList());
+
+
+        produitCommanderService.saveAll(produitCommanders);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/com/example/quickdinner/controller/UserController.java
+++ src/main/java/com/example/quickdinner/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.quickdinner.controller;
 
-import com.example.quickdinner.model.*;
-import com.example.quickdinner.service.*;
+import com.example.quickdinner.model.Panier;
+import com.example.quickdinner.model.Utilisateur;
+import com.example.quickdinner.service.PanierService;
+import com.example.quickdinner.service.RoleService;
+import com.example.quickdinner.service.UtilisateurService;
 import com.example.quickdinner.utils.Jwt;
-import com.example.quickdinner.utils.PairPoduitQuantite;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import io.swagger.annotations.ApiImplicitParam;
@@ -14,11 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api")
@@ -28,20 +26,12 @@ public class UserController {
     private final UtilisateurService utilisateurService;
     private final RoleService roleService;
     private final PanierService panierService;
-    private final CommandeService commandeService;
-    private final ProduitCommanderService produitCommanderService;
-    private final ProduitService produitService;
 
     public UserController(UtilisateurService utilisateurService,
-                          RoleService roleService, PanierService panierService,
-                          CommandeService commandeService, ProduitCommanderService produitCommanderService,
-                          ProduitService produitService) {
+                          RoleService roleService, PanierService panierService) {
         this.utilisateurService = utilisateurService;
         this.roleService = roleService;
         this.panierService = panierService;
-        this.commandeService = commandeService;
-        this.produitCommanderService = produitCommanderService;
-        this.produitService = produitService;
     }
 
     @ApiOperation("Enregistre un nouvelle utilisateur")
@@ -147,67 +137,5 @@ public class UserController {
         }
 
         return ResponseEntity.ok(connectedUser.getPanier());
-    }
-
-    @ApiOperation("Récupère les commandes d'un utilisateur: Si l'utilisateur est un client")
-    @GetMapping("/user/commandes")
-    public ResponseEntity<List<Commande>> getCommandes(@RequestHeader("Authorization") String token) {
-        Optional<Utilisateur> user = Jwt.getUserFromToken(token, utilisateurService);
-        if(user == null || !user.isPresent()) {
-            return ResponseEntity.badRequest().body(null);
-        }
-
-        Utilisateur connectedUser = user.get();
-
-        if("Client".equals(connectedUser.getRole().getLibelle())) {
-            return ResponseEntity.ok(commandeService.findAllByUtilisateurId(connectedUser.getId()));
-        }
-
-        return ResponseEntity.status(401).body(null);
-    }
-
-    @ApiOperation("Permet à l'utilisateur de passer une commande")
-    @PostMapping("/user/commandes")
-    public ResponseEntity createCommande(@RequestHeader("Authorization") String token, @RequestBody List<PairPoduitQuantite> produits) {
-        Optional<Utilisateur> user = Jwt.getUserFromToken(token, utilisateurService);
-        if(user == null || !user.isPresent()) {
-            return ResponseEntity.badRequest().body(null);
-        }
-
-        Utilisateur connectedUser = user.get();
-
-        if(!"Client".equals(connectedUser.getRole().getLibelle())) {
-            return ResponseEntity.status(401).body(null);
-        }
-
-        Commande commande = Commande.builder()
-                .utilisateur(connectedUser)
-                .build();
-
-        commande = commandeService.save(commande);
-        Map<Integer, Produit> produitsMap = new HashMap<>();
-
-        Commande finalCommande = commande;
-        List<ProduitCommander> produitCommanders = produits.stream()
-                        .filter(pairPoduitQuantite -> {
-                                Produit produit = produitService.findById(pairPoduitQuantite.getId()).orElse(null);
-                                if(produit != null) {
-                                    produitsMap.put(pairPoduitQuantite.getId(), produit);
-                                    return true;
-                                }
-                                return false;
-                        })
-                        .map(pairPoduitQuantite ->
-                                ProduitCommander.builder()
-                                    .commande(finalCommande)
-                                    .produit(produitsMap.get(pairPoduitQuantite.getId()))
-                                    .quantite(pairPoduitQuantite.getQuantite())
-                                    .build())
-                        .collect(Collectors.toList());
-
-
-        produitCommanderService.saveAll(produitCommanders);
-
-        return ResponseEntity.ok().build();
     }
 }
